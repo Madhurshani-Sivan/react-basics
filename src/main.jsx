544f67a9d72d889206aca47a81d51a67ea88a674
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-09-08 21:09:01 +0530

    add layout routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,17 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import NewPost from "./components/NewPost";
+import RootLayout from "./routes/RootLayout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/create-post", element: <NewPost /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/create-post", element: <NewPost /> },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
